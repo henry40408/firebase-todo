02e32925c87c1637784048a8acc6e69f181613bc
Heng-Yi Wu <henry40408@gmail.com>
2016-12-07 15:04:53 +0800

    Login form

--- app/initialize.jsx
+++ app/initialize.jsx
@@ -3,14 +3,37 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Button,
+  Container,
+  Form,
+  Grid,
+  Input,
+  Segment,
+} from 'semantic-ui-react';
 
 const TodoApp = () => (
-  <div className="ui todo app container">
+  <Container className="todo app">
     <h1 className="todo app header">
       {'Todo App'}
       <small>{'on Firebase'}</small>
     </h1>
-  </div>
+    <Grid centered columns={2}>
+      <Grid.Column>
+        <Form>
+          <Segment>
+            <Form.Field>
+              <Input placeholder={'E-mail'} />
+            </Form.Field>
+            <Form.Field>
+              <Input type={'password'} placeholder={'Password'} />
+            </Form.Field>
+            <Button primary>{'Login'}</Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  </Container>
 );
 
 $(document).ready(() => {
